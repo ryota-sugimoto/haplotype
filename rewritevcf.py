3619c90397171a8d70edb5222cd022cc9499e036
Ryota Sugimoto <ryota.sugimoto@gmail.com>
2012-11-30 16:07:24 +0900

    changed rewirtevcf

--- rewritevcf.py
+++ rewritevcf.py
@@ -5,41 +5,54 @@ def genotype(line):
   return line.split("\t")[9].split(":")[0]
 
 def refReadNum(line):
-  return int(line.split("\t")[9].split(":")[1].split(",")[0])
+  return float(line.split("\t")[9].split(":")[1].split(",")[0])
 
 def altReadNum(line):
-  return int(line.split("\t")[9].split(":")[1].split(",")[1])
+  return float(line.split("\t")[9].split(":")[1].split(",")[1])
 
-def isAA(line):
-  return refReadNum(line) > altReadNum(line)
+def isAA(line, threshold):
+  return altReadNum(line) / (refReadNum(line) + altReadNum(line)) < threshold
 
-def isBB(line):
-  return refReadNum(line) < altReadNum(line)
+def isBB(line, threshold):
+  return altReadNum(line) / (refReadNum(line) + altReadNum(line)) >= threshold
 
-def rewrite2BB(line):
+def rewrite2BB(line, bb_threshold):
   if re.match("^#", line):
     return line
-  else:
+  if isBB(line, bb_threshold):
     splited = line.split("\t")
     gen = splited[9].split(":")
     gen[0] = "1/1"
     splited[9] = ":".join(gen)
     return "\t".join(splited)
+  else:
+    return line
 
-def rewriteVcf(file):
-  return "\n".join(map(lambda line: line.strip(),
-                     map(rewrite2BB, 
-                         filter(lambda line: re.match("^#",line) or isBB(line),
-                                file))))
+def rewriteVcf(file, aa_threshold, bb_threshold):
+  file.seek(0)
+  header = filter(lambda line: re.match("^#", line), file)
+  file.seek(0)
+  data = filter(lambda line: not re.match("^#", line), file)
+  
+  pf = lambda line: not isAA(line, aa_threshold)
+  rewrited_data = map(lambda line: rewrite2BB(line, bb_threshold),
+                      filter(pf,data))
+  return "\n".join(map(lambda line: line.strip(), header + rewrited_data))
  
 def parse():
   import argparse
   
   parser = argparse.ArgumentParser()
   parser.add_argument("vcf_file")
+  parser.add_argument("-a", "--aa_threshold",
+                      default=0.3,
+                      type=float)
+  parser.add_argument("-b", "--bb_threshold",
+                      default=0.8,
+                      type=float)
   return parser.parse_args()
 
 if __name__ == "__main__":
   args = parse()
   vcf = open(args.vcf_file)
-  print rewriteVcf(vcf)
+  print rewriteVcf(vcf, args.aa_threshold, args.bb_threshold)
